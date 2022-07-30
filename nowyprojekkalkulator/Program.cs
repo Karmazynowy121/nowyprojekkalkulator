f77abd7348b815a4bb044199d4435006ed19b75a
Karmazynowy121 <atak121@wp.pl>
2022-07-30 20:52:00 +0200

    Initial commit

---
+++ nowyprojekkalkulator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nowyprojekkalkulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj Liczbe:  ");
+            int Liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj kolejna liczbe:  ");
+            int Liczba2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Liczba1 + Liczba2);
+
+            Console.ReadLine();
+        }
+    }
+}
